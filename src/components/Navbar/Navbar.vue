168745e9f32289fa89ac35a1c44a00859a803fb7
jaye <jaye1248276@gmail.com>
2023-04-27 19:29:33 +0800

    [feat]: 添加Navbar全局组件

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <uni-nav-bar
+    class="font-bold"
+    title="MOO"
+    background-color="#1a191b"
+    color="#fffeff"
+    :border="false"
+    v-bind="props"
+  />
+</template>
+
+<script setup lang="ts">
+import type { UniNavBarProps } from '@uni-helper/uni-ui-types'
+interface NavBarProps extends UniNavBarProps {}
+
+const props = defineProps<NavBarProps>()
+
+</script>
