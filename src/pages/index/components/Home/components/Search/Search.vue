cc317e4262a0b1d5a926a97679e3c8565d545f92
jaye <jaye1248276@gmail.com>
2023-04-27 21:58:41 +0800

    [feat]: Search组件完成

---
+++ src/pages/index/components/Home/components/Search/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="relative">
+    <uni-easyinput
+      v-model="search"
+      class="rounded-[30rpx] overflow-hidden indent-[54rpx]"
+      type="text"
+      confirm-type="search"
+      :clearable="false"
+      :input-border="false"
+      :styles="{ backgroundColor: '#010001', color: '#fce939', disableColor: '#eee', borderColor: 'transparent' }"
+    />
+    <uni-icons
+      custom-prefix="iconfont"
+      type="icon-search"
+      class="!text-[60rpx] !text-yellow-1 absolute top-[50%] translate-y-[-50%] ml-1"
+    />
+  </view>
+</template>
+
+<script setup lang="ts">
+const search = ref('')
+</script>
