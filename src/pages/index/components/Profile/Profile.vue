291a45c12f3043942ff687c4defdfc2bc84b3414
jaye <jaye1248276@gmail.com>
2023-05-01 20:20:19 +0800

    [feat]: profile页面完成

--- src/pages/index/components/Profile/Profile.vue
+++ src/pages/index/components/Profile/Profile.vue
@@ -1,9 +1,47 @@
 <template>
-  <view>
-    s
+  <view class="w-screen h-full box-border px-[28rpx]">
+    <view class="h-[300rpx] p-[30rpx] rounded-[20rpx] bg-black-1 text-[38rpx] box-border flex flex-col justify-between font-bold">
+      <view class="text-white-1">
+        <JIcon custom-class="icon-user mr-[20rpx]" />
+        <text class="name">Jaye</text>
+      </view>
+    <!--<view class="flex justify-between items-center text-grey-1">
+      <view class="text">
+        <text>关注</text>
+        <text class="count">0</text>
+      </view>
+      <view class="text">
+        <text>粉丝</text>
+        <text class="count">0</text>
+      </view>
+      <view class="vip">
+        <text class="text">免费VIP活动</text>
+      </view>
+    </view>-->
+    </view>
+
+    <view class="flex flex-wrap justify-around items-center mt-5">
+      <Shelf
+        v-for="item in shelf"
+        :key="item.description"
+        :description="item.description"
+        :count="item.count"
+        :icon="item.icon"
+        :list="item.list"
+        :url="item.url"
+      />
+    </view>
   </view>
 </template>
 
 <script setup lang="ts">
+import type { IShelf } from './components/Shelf/Shelf.vue'
+import Shelf from './components/Shelf/Shelf.vue'
 
+const shelf = ref<IShelf[]>([
+  { description: '收藏音乐', count: 0, icon: 'heart', list: [], url: './profile/collectSongs/index' },
+  { description: '收藏歌单', count: 0, icon: 'playlist', list: [], url: './profile/collectPlaylist/index' },
+  { description: '本地歌曲', count: 0, icon: 'file', list: [], url: '' },
+  { description: '最近播放', count: 0, icon: 'time', list: [], url: './profile/historyPlays/index' }
+])
 </script>
---
+++ src/pages/index/components/Profile/components/Shelf/Shelf.vue
@@ -0,0 +1,67 @@
+<template>
+  <view
+    class="w-[330rpx] h-[220rpx] relative"
+    catch:tap="toNavigate"
+  >
+    <view class="relative flex items-end w-[88%] h-[60%] mx-auto">
+      <template
+        v-for="index in 3"
+        :key="index"
+      >
+        <image
+          v-if="list[index]"
+          :src="list[index]"
+          class="absolute w-[50%] h-full"
+          :class="'pic-' + index"
+        />
+        <view
+          wx:else
+          class="absolute w-[50%] h-full bg-grey-1 shadow-[4rpx_0_6rpx_#666]"
+          :class="'pic-' + index"
+        >
+          <JIcon
+            v-if="index === 1"
+            :type="'icon-' + icon"
+            custom-class="text-white-1 text-[80rpx] mid"
+          />
+        </view>
+      </template>
+    </view>
+
+    <view class="w-full rounded-[20rpx] py-[12rpx] px-[20rpx] box-border bg-black-1 flex justify-between items-center font-bold">
+      <text class="text-white-1">{{ description }}</text>
+      <text class="text-grey-1">{{ count }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+export interface IShelf {
+  description: string
+  count: number
+  icon: string
+  list: string[]
+  url: string
+}
+
+defineProps<IShelf>()
+</script>
+
+<style scoped lang="scss">
+.pic-1 {
+  left: 0;
+  z-index: 3;
+}
+.pic-2 {
+  height: 90%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2;
+}
+.pic-3 {
+  height: 90%;
+  left: 100%;
+  transform: translateX(-100%);
+  z-index: 1;
+}
+</style>
