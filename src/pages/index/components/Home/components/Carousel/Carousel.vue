231bbaaba37cbc4e160422cf0e8342bc7a8e0429
jaye <jaye1248276@gmail.com>
2023-05-04 23:55:46 +0800

    [update]: 优化carousel性能

--- src/pages/index/components/Home/components/Carousel/Carousel.vue
+++ src/pages/index/components/Home/components/Carousel/Carousel.vue
@@ -1,30 +1,32 @@
 <template>
-  <swiper
-    class="rounded-[14rpx] overflow-hidden"
-    :autoplay="autoplay"
-    :duration="400"
-    :interval="4000"
-    circular
-    indicator-dots
-    indicator-color="#ABA8A8"
-    indicator-active-color="#fff"
-    @change="onChange"
-  >
-    <swiper-item
-      v-for="(item, index) in bannerList"
-      :key="index"
-      skip-hidden-item-layout
+  <view>
+    <swiper
+      class="carousel rounded-[14rpx] overflow-hidden"
+      :autoplay="autoplay"
+      :duration="400"
+      :interval="4000"
+      circular
+      indicator-dots
+      indicator-color="#ABA8A8"
+      indicator-active-color="#fff"
+      @change="onChange"
     >
-      <JImage
-        :id="index"
-        :src="item.pic"
-        :lazy-load="currentIndex !== index"
-        width="100%"
-        height="100%"
-        radius="14rpx"
-      />
-    </swiper-item>
-  </swiper>
+      <swiper-item
+        v-for="(item, index) in bannerList"
+        :key="index"
+        skip-hidden-item-layout
+      >
+        <JImage
+          :id="index"
+          :src="item.pic"
+          :lazy-load="currentIndex !== index"
+          width="100%"
+          height="100%"
+          radius="14rpx"
+        />
+      </swiper-item>
+    </swiper>
+  </view>
 </template>
 
 <script setup lang="ts">
@@ -40,6 +42,17 @@ fetchBanner()
 
 onShow(() => { autoplay.value = true })
 onHide(() => { autoplay.value = false })
+
+onMounted(() => {
+  const observer = uni.createIntersectionObserver(getCurrentInstance()?.proxy)
+  observer.relativeToViewport({ bottom: 100 }).observe(`.carousel`, (res) => {
+    autoplay.value = false
+
+    if (res.intersectionRatio === 0) return
+
+    autoplay.value = true
+  })
+})
 
 function onChange({ detail: { current }}: SwiperOnChangeEvent) {
   // console.log('🚀 ~ file: Carousel.vue:42 ~ onChange ~ e:', current)
