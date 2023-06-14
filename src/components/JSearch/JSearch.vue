384fe43d60178a677fb2a68d3e3f89cf15e9ba6b
jaye <jaye1248276@gmail.com>
2023-06-14 12:24:39 +0800

    [feat]: JSearch全局组件新增cancel功能button

--- src/components/JSearch/JSearch.vue
+++ src/components/JSearch/JSearch.vue
@@ -1,46 +1,63 @@
 <template>
   <view
-    class="flex justify-center items-center px-2 py-[4rpx] m-[20rpx]"
+    class="box-border flex items-center mx-[28rpx] my-[20rpx]"
     :class="customClass"
-    :style="{borderRadius: radius, backgroundColor: bgColor }"
-    @tap="isActive = true"
   >
-    <slot name="searchIcon">
-      <uni-icons
-        color="#c0c4cc"
-        size="18"
-        type="search"
-      />
-    </slot>
-
-    <input
-      v-if="isActive"
-      :value="modelValue"
-      class="flex-1 text-[30rpx] mx-2"
-      :placeholder="placeholder"
-      :placeholder-style="placeholderStyle"
-      :focus="focus || isActive"
-      :disabled="readonly"
-      :maxlength="maxlength"
-      confirm-type="search"
-      type="text"
-      :style="{ color }"
-      @input="$emit('update:modelValue', $event.detail.value)"
-    >
-
     <view
-      v-if="isActive && clearButton"
-      v-show="modelValue"
-      @tap="$emit('update:modelValue', ''); emit('clear')"
+      class="flex-1 flex justify-center items-center px-2 py-[4rpx]"
+      :style="{borderRadius: radius, backgroundColor: bgColor }"
+      @tap="$emit('click')"
     >
-      <slot name="clearIcon">
+      <slot name="searchIcon">
         <uni-icons
           color="#c0c4cc"
-          size="20"
-          type="clear"
+          size="18"
+          type="search"
         />
       </slot>
+
+      <input
+        v-if="!readonly || focus || placeholder"
+        :value="modelValue"
+        class="flex-1 text-[30rpx] mx-2"
+        :placeholder="placeholder"
+        :placeholder-style="placeholderStyle"
+        :focus="focus"
+        :disabled="readonly"
+        :maxlength="maxlength"
+        confirm-type="search"
+        type="text"
+        :style="{ color }"
+        v-bind="$attrs"
+        @input="$emit('update:modelValue', $event.detail.value)"
+      >
+
+      <view
+        v-if="clearButton"
+        v-show="modelValue"
+        @tap="$emit('update:modelValue', ''); $emit('clear')"
+      >
+        <slot name="clearIcon">
+          <uni-icons
+            color="#c0c4cc"
+            size="20"
+            type="clear"
+          />
+        </slot>
+      </view>
     </view>
+
+    <slot
+      v-if="cancelButton"
+      name="cancel"
+    >
+      <view
+        class="ml-[28rpx] text-white-1"
+        @tap="$emit('update:modelValue', ''); $emit('cancel')"
+      >
+        取消
+      </view>
+    </slot>
   </view>
 </template>
 
@@ -56,6 +73,7 @@ interface JSearchProps {
   readonly?: boolean
   maxlength?: number
   clearButton?: boolean
+  cancelButton?: boolean
   customClass?: string
 }
 
@@ -69,12 +87,16 @@ withDefaults(defineProps<JSearchProps>(), {
   readonly: false,
   maxlength: 140,
   clearButton: true,
+  cancelButton: false,
   customClass: ''
 })
-const emit = defineEmits<{
+defineEmits<{
   (e: 'update:modelValue', value: string): void
   (e: 'clear'): void
+  (e: 'click'): void
+  (e: 'focus'): void
+  (e: 'confirm'): void
+  (e: 'blur'): void
+  (e: 'cancel'): void
 }>()
-
-const isActive = ref(false)
 </script>
