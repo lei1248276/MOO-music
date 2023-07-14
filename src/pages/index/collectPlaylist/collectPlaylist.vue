cbb4f21fcb7756267ebfd68578ec600e91f32a71
jaye <jaye1248276@gmail.com>
2023-07-14 21:47:26 +0800

    fix: 修复小程序sticky定位无效问题

--- src/pages/index/collectPlaylist/collectPlaylist.vue
+++ src/pages/index/collectPlaylist/collectPlaylist.vue
@@ -5,31 +5,32 @@
     @click-left-icon="isShowPage = false"
   />
 
+  <Subtitle
+    icon="icon-more"
+    class="z-50 fixed left-0 right-0 bg-black-2 px-[28rpx]"
+    custom-class="my-0"
+    :style="{ top: 44 + useStatusBarHeight().value + 'px' }"
+  >
+    <template #title>
+      <view class="flex items-center">
+        <button
+          class="h-[64rpx] !leading-[64rpx] m-0 rounded-full bg-yellow-1 text-black-1"
+          size="mini"
+        >
+          <JIcon custom-class="icon-play text-[42rpx]" />
+        </button>
+
+        <text class="text-grey-1 inline-block align-middle ml-5 font-[38rpx]">
+          {{ cacheStore.collectPlaylist.length }}
+        </text>
+      </view>
+    </template>
+  </Subtitle>
+
   <!-- #ifdef H5 -->
   <H5BackTransition :show="isShowPage">
     <!-- #endif -->
-    <view class="bg-black-2 px-[28rpx] after:block after:pb-[calc(150rpx_+_env(safe-area-inset-bottom))]">
-      <Subtitle
-        icon="icon-more"
-        class="z-50 sticky top-[44px] left-0 right-0 bg-black-2"
-        :style="{ top: 44 + statusBarHeight + 'px' }"
-      >
-        <template #title>
-          <view class="flex items-center">
-            <button
-              class="h-[64rpx] !leading-[64rpx] m-0 rounded-full bg-yellow-1 text-black-1"
-              size="mini"
-            >
-              <JIcon custom-class="icon-play text-[42rpx]" />
-            </button>
-
-            <text class="text-grey-1 inline-block align-middle ml-5 font-[38rpx]">
-              {{ cacheStore.collectPlaylist.length }}
-            </text>
-          </view>
-        </template>
-      </Subtitle>
-
+    <view class="bg-black-2 px-[28rpx] mt-[106rpx] after:block after:pb-[calc(150rpx_+_env(safe-area-inset-bottom))]">
       <uni-swipe-action class="animate-enter-content">
         <uni-swipe-action-item
           v-for="(playlist, index) in lazyList"
@@ -75,7 +76,6 @@
 import type { Playlist } from '@/api/interface/Playlist'
 
 const cacheStore = useCacheStore()
-const statusBarHeight = useStatusBarHeight()
 
 // #ifdef H5
 const isShowPage = ref(true)
--- src/pages/index/collectSongs/collectSongs.vue
+++ src/pages/index/collectSongs/collectSongs.vue
@@ -5,33 +5,34 @@
     @click-left-icon="isShowPage = false"
   />
 
+  <Subtitle
+    icon="icon-menu"
+    icon-size="60rpx"
+    class="z-50 fixed left-0 right-0 bg-black-2 px-[28rpx]"
+    custom-class="my-0"
+    :style="{ top: 44 + useStatusBarHeight().value + 'px' }"
+  >
+    <template #title>
+      <view class="flex items-center">
+        <button
+          class="h-[64rpx] !leading-[64rpx] m-0 rounded-full bg-yellow-1 text-black-1"
+          size="mini"
+          @tap="onSong(0)"
+        >
+          <JIcon custom-class="icon-play text-[42rpx]" />
+        </button>
+
+        <text class="text-grey-1 inline-block align-middle ml-5 font-[38rpx]">
+          {{ cacheStore.collectSongs.length }}
+        </text>
+      </view>
+    </template>
+  </Subtitle>
+
   <!-- #ifdef H5 -->
   <H5BackTransition :show="isShowPage">
     <!-- #endif -->
-    <view class="bg-black-2 px-[28rpx] after:block after:pb-[calc(150rpx_+_env(safe-area-inset-bottom))]">
-      <Subtitle
-        icon="icon-menu"
-        icon-size="60rpx"
-        class="z-50 sticky top-[44px] left-0 right-0 bg-black-2"
-        :style="{ top: 44 + statusBarHeight + 'px' }"
-      >
-        <template #title>
-          <view class="flex items-center">
-            <button
-              class="h-[64rpx] !leading-[64rpx] m-0 rounded-full bg-yellow-1 text-black-1"
-              size="mini"
-              @tap="onSong(0)"
-            >
-              <JIcon custom-class="icon-play text-[42rpx]" />
-            </button>
-
-            <text class="text-grey-1 inline-block align-middle ml-5 font-[38rpx]">
-              {{ cacheStore.collectSongs.length }}
-            </text>
-          </view>
-        </template>
-      </Subtitle>
-
+    <view class="bg-black-2 px-[28rpx] mt-[106rpx] after:block after:pb-[calc(150rpx_+_env(safe-area-inset-bottom))]">
       <uni-swipe-action class="animate-enter-content">
         <uni-swipe-action-item
           v-for="(song, index) in lazyList"
@@ -62,7 +63,6 @@ import type { Song } from '@/components/Song/Song.vue'
 
 const audioStore = useAudioStore()
 const cacheStore = useCacheStore()
-const statusBarHeight = useStatusBarHeight()
 
 // #ifdef H5
 const isShowPage = ref(true)
