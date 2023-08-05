b42241ba25560444d6db4585d5144ceb8b3c1476
jaye <jaye1248276@gmail.com>
2023-08-05 13:59:57 +0800

    refactor: 重构useNavigateTo取消自动返回

--- src/hooks/useNavigateTo.ts
+++ src/hooks/useNavigateTo.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-redeclare */
 
 /**
- * @description 跳转时会判断前后页面是否相同，current === to 取消跳转，from === to 返回跳转
+ * @description 跳转时会判断前后页面是否相同，current === to 取消跳转
  * @param url 跳转URL
  */
 export default function useNavigateTo(url: string): void
 
 /**
- * @description 跳转时会判断前后页面是否相同，current === to 取消跳转，from === to 返回跳转
+ * @description 跳转时会判断前后页面是否相同，current === to 取消跳转
  * @param options uni.navigateTo相同参数
  */
 export default function useNavigateTo(options: Parameters<typeof uni.navigateTo>[0]): void
@@ -18,7 +18,7 @@ export default function useNavigateTo(url: string | Parameters<typeof uni.naviga
   if (pages.length && pages.pop().$page.fullPath === (url.url || url)) return
 
   // @ts-ignore // * fromRoute === toRoute 直接返回
-  if (pages.length && pages.pop().$page.fullPath === (url.url || url)) return uni.navigateBack()
+  // if (pages.length && pages.pop().$page.fullPath === (url.url || url)) return uni.navigateBack()
 
   uni.navigateTo(typeof url === 'string'
     ? { url, fail: (err) => { console.error(err) } }
