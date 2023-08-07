5c7f98bcf7cc5928cf4843830d271b7fbd714311
jaye <jaye1248276@gmail.com>
2023-08-07 22:28:14 +0800

    fix: 解决小程序cookie失效问题

--- src/api/play.ts
+++ src/api/play.ts
@@ -1,10 +1,16 @@
 import request from '@/utils/request'
+import { MOO_API } from '@/utils/request'
 import type { SongURLResponse } from '@/api/interface/SongURL'
 import type { LyricResponse } from '@/api/interface/Lyric'
 
 // * 获取歌曲url
 export function getSongURL(id: number) {
-  return request.get<any, SongURLResponse>(`/song/url?id=${id}`)
+  return request.get<any, SongURLResponse>(`${MOO_API}/song/url/v1?id=${id}&level=lossless`,
+    {
+    // #ifdef MP-WEIXIN
+      header: { cookie: uni.getStorageSync('cookie') || '' }
+    // #endif
+    })
 }
 
 // * 获取歌曲歌词
