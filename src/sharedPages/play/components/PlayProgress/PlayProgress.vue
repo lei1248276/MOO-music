a82c208b4f04e72018238ee13d0d9deaff2aef0b
Jaye <jaye1248276@gmail.com>
2023-09-27 23:59:40 +0800

    fix: 修复微信小程序PlayProgress组件兼容问题

--- src/sharedPages/play/components/PlayProgress/PlayProgress.vue
+++ src/sharedPages/play/components/PlayProgress/PlayProgress.vue
@@ -6,12 +6,31 @@
     background-color="rgba(255,255,255,.3)"
     block-color="transparent"
     :block-size="20"
-    class="progress x-mid bottom-[18%] w-4/5 m-0 scale-y-[2.0] duration-300 transition-transform"
+    class="progress z-50 x-mid bottom-[18%] w-4/5 m-0 scale-y-[2.0] duration-300 transition-transform"
     :class="{'!scale-y-[3.5] !duration-500 !rounded-full': isTouch}"
     @changing="onChanging"
     @change="onChange"
   />
+  <!-- #ifdef MP-WEIXIN -->
+  <view
+    class="x-mid bottom-[18%] w-4/5 h-[12rpx] bg-[rgba(255,255,255,.3)] rounded-full"
+    :class="{'scale-y-150 !duration-300': isTouch}"
+  >
+    <view
+      class="h-full bg-white-1 rounded-full"
+      :style="{width: audioStore.currentTime / audioStore.duration * 100 + '%'}"
+    />
+  </view>
+  <!-- #endif -->
 </template>
+
+<script lang="ts">
+export default {
+  options: {
+    virtualHost: true
+  }
+}
+</script>
 
 <script setup lang="ts">
 import type { SliderOnChangingEvent, SliderOnChangeEvent } from '@uni-helper/uni-app-types'
@@ -42,4 +61,10 @@ function onChange({ detail: { value }}: SliderOnChangeEvent) {
 .progress :deep(.uni-slider-thumb){
   box-shadow: none !important;
 }
+
+/* #ifdef MP-WEIXIN */
+slider{
+  opacity: 0 !important;
+}
+/* #endif */
 </style>
