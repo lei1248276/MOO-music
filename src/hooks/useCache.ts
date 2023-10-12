73d50ac92463ffcfbb7a69f0b8ae94d896c5942b
Jaye <jaye1248276@gmail.com>
2023-10-12 13:59:59 +0800

    refactor: 重构useCache代码逻辑

--- src/hooks/useCache.ts
+++ src/hooks/useCache.ts
@@ -3,7 +3,9 @@
  * @param key 缓存key
  * @param fallback 初始值，获取到缓存后会更新初始值
  */
-export default function useCache<T>(key: string, fallback: T) {
+export default function useCache<
+  T extends ReturnType<typeof ref> | ReturnType<typeof shallowRef> | ReturnType<typeof reactive> | ReturnType<typeof shallowReactive>
+>(key: string, fallback: T) {
   uni.getStorage({
     key,
     success({ data }) {
@@ -11,35 +13,28 @@ export default function useCache<T>(key: string, fallback: T) {
 
       if (isRef(fallback)) {
         fallback.value = data
-
-        watch(fallback, (value) => {
-          uni.setStorage({
-            key,
-            data: value,
-            fail(err) { console.error(err) }
-          })
-        })
-
         return
       }
 
       if (isReactive(fallback)) {
         Array.isArray(data) && Array.isArray(fallback)
           ? data.forEach((v, i) => { fallback[i] = v })
-          : Object.assign(fallback as object, data)
-
-        watch(() => fallback, (value) => {
-          uni.setStorage({
-            key,
-            data: value,
-            fail(err) { console.error(err) }
-          })
-        }, { deep: true })
-
+          : Object.assign(fallback, data)
         return
       }
     },
-    fail(err) { console.error(err) }
+    fail(err) {
+      console.error(err)
+    },
+    complete() {
+      watch(fallback, (value) => {
+        uni.setStorage({
+          key,
+          data: value,
+          fail(err) { console.error(err) }
+        })
+      })
+    }
   })
 
   return fallback
